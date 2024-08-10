a3cc9b79b6c4d190f919733caf5f7d1281046811
Serhat <genc.serhat97@gmail.com>
2024-08-10 15:43:18 +0300

    🎨 Add another demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { countStore } from "./global";
+import { countStore, textStore } from "./global";
 import { derived } from "./store";
 import { useStore } from "./store/hooks/useStore";
 
 function App() {
   const [count, setCount] = useStore(countStore);
-  const [countSquared] = useStore(
-    derived((state) => state * state, countStore)
+  const derivedCountStore = derived((state) => state * state, countStore);
+
+  const [countSquared] = useStore(derivedCountStore);
+
+  const [{ name, surname }, setText] = useStore(textStore);
+  const derivedTextStore = derived(
+    ({ name, surname }) => `${name}${surname}`.length,
+    textStore
   );
+  const [textLength] = useStore(derivedTextStore);
 
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
@@ -19,10 +26,53 @@ function App() {
         }}
       >
         <h3>State Manager Demo</h3>
-        <button onClick={() => setCount((prev) => prev + 1)}>Increase</button>
-        <div>Count: {count}</div>
-        <div>Count Squared: {countSquared}</div>
-        <button onClick={() => setCount((prev) => prev - 1)}>Decrease</button>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <button onClick={() => setCount((prev) => prev + 1)}>Increase</button>
+          <div>Count: {count}</div>
+          <div>Count Squared: {countSquared}</div>
+          <button onClick={() => setCount((prev) => prev - 1)}>Decrease</button>
+        </div>
+        <hr style={{ width: "100%" }} />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <input
+            type="text"
+            placeholder="name"
+            onChange={(e) =>
+              setText((prev) => ({ ...prev, name: e.target.value }))
+            }
+            maxLength={20}
+          />
+          <input
+            type="text"
+            maxLength={20}
+            placeholder="surname"
+            onChange={(e) =>
+              setText((prev) => ({ ...prev, surname: e.target.value }))
+            }
+          />
+          {textLength > 0 ? (
+            <div>
+              <p>
+                Your name is: <b>{`${name} ${surname}`}</b>
+              </p>
+              <p>
+                Name surname combined text length: <b>{textLength}</b>
+              </p>
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
   );
--- src/global.ts
+++ src/global.ts
@@ -1,3 +1,9 @@
 import { Store } from "./store";
 
+type TextStoreType = { name: string; surname: string };
+
 export const countStore = new Store(0);
+export const textStore = new Store<TextStoreType>({
+  name: "",
+  surname: "",
+});
